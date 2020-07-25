8262d050d67ec5177a72991fc6ab3d3f6be45551
dcervantesr <dcervantesr@gmail.com>
2020-07-25 08:41:50 -0300

    clase 6 actualizada

--- clase6.js
+++ clase6.js
@@ -12,30 +12,30 @@ $(function() {
 
     // Manipulación del DOM
     // text()    html()  val()
-    // $("#btntexto").click(function() {
-    //     $("#texto").text("Ha hecho clic en el boton Texto");
-    // });
+    $("#btntexto").click(function() {
+        $("#texto").text("Ha hecho clic en el boton Texto");
+    });
 
-    // $("#btnhtml").click(function() {
-    //     $("#texto").html("<span>Ha hecho clic en el boton Html</span>");
-    // });
+    $("#btnhtml").click(function() {
+        $("#texto").html("<span>Ha hecho clic en el boton Html</span>");
+    });
 
     // acceder a valores de atributos
     // attr()
     $("#btntexto").click(function() {
         // $(this).addClass("color");
         // $(this).removeClass("color");
-        $(this).toggleClass("color");
-        $(this).css({ "background-color": "black", "padding": "5px" });
+        // $(this).toggleClass("color");
+        // $(this).css({ "background-color": "black", "padding": "5px" });
         // $("#texto").attr({ "style": "color: blue" });
         // $("#texto").append(" :) ");
         // $("#texto").prepend(" :) ");
         // $("#btnhtml").remove();
-        // $("#div1").empty();
+        // ("#div1").empty();
         // $("#texto").addClass("color_fondo");
     });
 
-    // Ejercicio (entrega antes del miércoles 15) dcervantes@ibec.edu.uy
+    // Ejercicio (entrega antes del jueves 30) dcervantes@ibec.edu.uy
     // Desarrollar una página Landing Page:
     // contenido libre
     // imagen de banner
